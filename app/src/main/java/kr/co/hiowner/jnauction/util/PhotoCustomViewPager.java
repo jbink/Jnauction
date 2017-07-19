be21e625b73562837a7b22df5107877d28bb1e95
jbink <jbink17@gmail.com>
2017-07-19 14:23:33 +0900

    commit

---
+++ app/src/main/java/kr/co/hiowner/jnauction/util/PhotoCustomViewPager.java
@@ -0,0 +1,39 @@
+package kr.co.hiowner.jnauction.util;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by user on 2017-07-08.
+ */
+public class PhotoCustomViewPager extends ViewPager {
+    public PhotoCustomViewPager(Context context) {
+        super(context);
+    }
+
+    public PhotoCustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
